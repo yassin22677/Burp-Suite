d96de3580c8b06d746a42855e25a3f026ddd441e
yassin22677 <yassinrizk9@gmail.com>
2025-10-07 17:36:19 +0300

    init

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,20 @@
-from flask import Flask
-from app.views.auth_routes import auth_bp
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
 
-    # Register blueprints
-    app.register_blueprint(auth_bp)
+    # Home route (for dashboard)
+    @app.route('/')
+    def home():
+        return render_template('dashboard.html')
 
     return app
+
+#from app.views.auth_routes import auth_bp
+
+
+
+    # Register blueprints
+   # app.register_blueprint(auth_bp)
+
+   
